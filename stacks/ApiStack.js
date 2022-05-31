26330b78b45860d1d014f9bb334ddd3540a9d9da
Tausif-TUF <engr.tausifkhan@gmail.com>
2022-05-30 18:48:01 -0700

    [feat]: add a route to insert a note in db

---
+++ stacks/ApiStack.js
@@ -0,0 +1,26 @@
+import { Api, use } from '@serverless-stack/resources'
+import { StorageStack } from './StorageStack'
+
+export function ApiStack({ stack, app}) {
+  const { table } = use(StorageStack);
+
+  // Create the API
+  const api = new Api(stack, 'Api', {
+    defaults: {
+      function: {
+        permissions: [table],
+        environment: {
+          TABLE_NAME: table.tableName,
+        },
+      },
+    },
+    routes: {
+      "POST /notes": "functions/create.main",
+    },
+  });
+
+  stack.addOutputs({
+    ApiEndpoint: api.url
+  })
+  return { api };
+};
